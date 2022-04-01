b36db3b1d51b92cdb2e46f687e376857c3711e21
donspb <donspb@gmail.com>
2022-04-01 15:59:16 +0300

    Init commit

--- app/src/main/java/ru/donspb/designit/MainActivity.kt
+++ app/src/main/java/ru/donspb/designit/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_container, FirstScreenFragment())
+                .commit()
+        }
+
     }
 }
