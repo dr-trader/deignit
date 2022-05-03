b1b15744e39f8974a90a02557a25813978ccaa23
donspb <donspb@gmail.com>
2022-05-03 21:27:09 +0300

    Tab Bar customization

--- app/src/main/java/ru/donspb/designit/ui/MainActivity.kt
+++ app/src/main/java/ru/donspb/designit/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.donspb.designit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.content.res.AppCompatResources
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -34,22 +35,37 @@ class MainActivity : AppCompatActivity() {
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle, tabsNames.size)
         viewPager.adapter = adapter
 
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val index = tab?.position
+                if (index != null) {
+                    tab.text = tabsNames[index]
+                    tab.icon?.setTint(getColor(R.color.bot_button_color_selected))
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.text = ""
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             when (position) {
                 0 -> {
-                    tab.text = getString(R.string.mi_home_name)
                     tab.icon = AppCompatResources.getDrawable(this, R.drawable.ic_home)
                 }
                 1 -> {
-                    tab.text = getString(R.string.mi_classes_name)
                     tab.icon = AppCompatResources.getDrawable(this, R.drawable.ic_calendar)
                 }
                 2 -> {
-                    tab.text = getString(R.string.mi_list_name)
                     tab.icon = AppCompatResources.getDrawable(this, R.drawable.ic_table)
                 }
                 3 -> {
-                    tab.text = getString(R.string.mi_fav_name)
                     tab.icon = AppCompatResources.getDrawable(this, R.drawable.ic_star)
                 }
             }
